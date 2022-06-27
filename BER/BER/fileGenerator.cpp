42b8ee5ef370723393612436106e6be420984158
Julia Kovalchuk <kovalchukjuls@gmail.com>
2022-06-27 18:31:44 +0200

    added fieles"

--- BER/BER/fileGenerator.cpp
+++ BER/BER/fileGenerator.cpp
@@ -1,4 +1,5 @@
 #include "fileGenerator.hpp"
+#include "binaryOps.hpp"
 
 void FileGenerator::generateFile(const std::string name, const char value, const int count)
 {
@@ -14,4 +15,84 @@ void FileGenerator::generateFile(const std::string name, const char value, const
 
         file.close();
     }
+}
+
+char* FileGenerator::getRandomBits(const int count)
+{
+    srand((unsigned)time(NULL));
+    int random = rand() % 5 + 2;
+    std::string suffix = std::to_string((count / random) + random);
+    std::string stringValue = "0x" + suffix;
+    char* charValue = new char[5];
+    strcpy_s(charValue, 5, stringValue.c_str());
+    return charValue;
+}
+
+void FileGenerator::generateRandomFile(const std::string name, const char value, const int count)
+{
+    std::ofstream file;
+    char* randomValue = new char[5];
+    strcpy_s(randomValue, 5, getRandomBits(count));
+    file.open(name.c_str(), std::ios::binary);
+
+    if (file.good())
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (i % 10 == 0)
+            {
+                file.write((char*)&randomValue, 1);
+            }
+            else {
+                file.write((char*)&value, 1);
+            }
+        }
+
+        file.close();
+    }
+}
+
+void FileGenerator::generateTestFiles()
+{
+    // test 1
+    std::cout << "  Generating 'test1_file1.bin'..." << std::endl;
+    FileGenerator::generateFile("test1_file1.bin", 0x55, 100);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    std::cout << "  Successsfully generated 'test1_file1.bin'." << std::endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Sleep(500);
+    std::cout << "  Generating 'test1_file2.bin'..." << std::endl;
+    FileGenerator::generateFile("test1_file2.bin", 0x55, 100);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    std::cout << "  Successsfully generated 'test1_file2.bin'." << std::endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Sleep(500);
+
+    // test 2
+    std::cout << "  Generating 'test2_file1.bin'..." << std::endl;
+    FileGenerator::generateFile("test2_file1.bin", 0x55, 100);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    std::cout << "  Successsfully generated 'test2_file1.bin'." << std::endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Sleep(500);
+    std::cout << "  Generating 'test2_file2.bin'..." << std::endl;
+    FileGenerator::generateRandomFile("test2_file2.bin", 0x55, 100);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    std::cout << "  Successsfully generated 'test2_file2.bin'." << std::endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Sleep(500);
+
+    // test 3
+    std::cout << "  Generating 'test3_file1.bin'..." << std::endl;
+    FileGenerator::generateFile("test3_file1.bin", 0x55, 400000000);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    std::cout << "  Successsfully generated 'test3_file1.bin'." << std::endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Sleep(500);
+    std::cout << "  Generating 'test3_file2.bin'..." << std::endl;
+    FileGenerator::generateFile("test3_file2.bin", 0x50, 400000000);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    std::cout << "  Successsfully generated 'test3_file2.bin'." << std::endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    Sleep(500);
 }
--- BER/BER/main.cpp
+++ BER/BER/main.cpp
@@ -1,12 +1,28 @@
-#include <iostream>
 #include "binaryOps.hpp"
 #include "fileGenerator.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "Bit Error Rate" << std::endl;
-    std::cout << "----------------------------------" << std::endl;
-    FileGenerator::generateFile("file1.bin", 0xFF, 100);
-    FileGenerator::generateFile("file2.bin", 0xFE, 100);
-    std::cout << int(BinaryOps::calculateHammingDistance(0xFF, 0xFE)) << std::endl;
+    std::string file1, file2;
+    berResults results;
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+
+    if (argc != 3)
+    {
+        std::cout << "No file names were provided." << std::endl;
+        Sleep(1000);
+        std::cout << "Generating test files..." << std::endl;
+        FileGenerator::generateTestFiles();
+        std::cout << "The generating has been completed.\nRe-run the program using the given arguments: './ber.exe test#_file1.bin test#_file2.bin'." << std::endl;
+    }
+    else
+    {
+        file1 = argv[1];
+        file2 = argv[2];
+
+        BinaryOps::calculateBER(file1, file2);
+    }
+
+    exit(0);
 }
